f2ac405a089bb05ae3198e8e393c87a0a12155e8
Felipe <oliveira8.1felipe@gmail.com>
2026-06-04 10:10:38 -0300

    working on new preloader

--- loader.js
+++ loader.js
@@ -1,29 +1,38 @@
 // Start by waiting for the DOM to be fully ready
 document.addEventListener("DOMContentLoaded", () => {
   
-  gsap.to(".img-preloader", {
-  rotationY: 360, // Rotates 180 degrees on the X-axis
-  repeat: -1,
-  duration: 1.5,
-  ease: "linear",
-  transformStyle: "preserve-3d" // Essential for 3D sub-elements
-});
-
-// A simple object to hold our number
-let counter = { val: 0 };
-const steps = [0, 23, 54, 74, 92, 100];
-const tl = gsap.timeline()
-
-tl.to(counter, { val: 12, duration: 0.5, snap: { val: 1 }, onUpdate: () => document.querySelector(".counter-display").innerText = counter.val + "%" })
-  .to(counter, { val: 23, duration: 0.5, snap: { val: 1 }, onUpdate: () => document.querySelector(".counter-display").innerText = counter.val + "%" })
-  .to(counter, { val: 54, duration: 0.3, snap: { val: 1 }, onUpdate: () => document.querySelector(".counter-display").innerText = counter.val + "%" })
-  .to(counter, { val: 74, duration: 0.3, snap: { val: 1 }, onUpdate: () => document.querySelector(".counter-display").innerText = counter.val + "%" })
-  .to(counter, { val: 92, duration: 0.5, snap: { val: 1 }, onUpdate: () => document.querySelector(".counter-display").innerText = counter.val + "%" })
-  .to(counter, { val: 100, duration: 0.5, snap: { val: 1 }, onUpdate: () => document.querySelector(".counter-display").innerText = counter.val + "%" })
-  .to(".preloader", {
-    autoAlpha:0,
-}, ">0.1")
-  .to(".img-preloader", {width: 0, duration: 2}, "<");
-
+  const tl = gsap.timeline()
+  const counterElement = document.querySelector(".counter");
+  const body = document.querySelector("body");
+  gsap.set(body, { overflowY: "hidden" });
   
-});
+  // 2. Define the starting and ending values
+  const myCounter = { val: 10 };
+
+  tl.from(".fill-line", {scaleX: 0, duration: 1})
+  .set(".loading-bar", {autoAlpha: 0}, ">")
+  .to(".counter-div", {display: "block"}, "<")
+  .to(myCounter, {
+    val: 0,                // Count down to 0
+    duration: 1.1,          // Duration in seconds
+    ease: "none",          // Linear countdown
+    roundProps: "val",     // Ensures it counts by integers (10, 9, 8...)
+    onUpdate: function() {
+        // Update the DOM text content on every frame
+        counterElement.textContent = myCounter.val;
+    }
+}, "<")
+  .set(".counter-div", {display: "none"}, ">")
+  .to(".cross-div", { display: "block" }, "<")
+  .to(".cross-div path", { fill: "#FFFFFF", duration: 0.1}, "<")
+  .to(".cross-div path", { fill: "#db0e00", duration: 0.1}, ">")
+  .to(".cross-div path", { fill: "#FFFFFF", duration: 0.1}, ">")
+  .to(".cross-div path", { fill: "#db0e00", duration: 0.1}, ">")
+  .to(".cross-div path", { fill: "#FFFFFF", duration: 0.1}, ">")
+  .to(".cross-div path", { fill: "#db0e00", duration: 0.1}, ">")
+  .to(".cross-div path", { fill: "#FFFFFF", duration: 0.1}, ">")
+  .to(".cross-div path", { fill: "#db0e00", duration: 0.1}, ">")
+  .to(".cross-div path", { fill: "#FFFFFF", duration: 0.1}, ">")
+  .to(".cross-div path", {scale: 20, duration: 1.2, transformOrigin: "center center"}, ">")
+  .to(".preloader", {autoAlpha: 0}, ">")
+})

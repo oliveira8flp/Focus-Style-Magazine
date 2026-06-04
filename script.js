5eccfee65344d19549f45f51662031d31d693d1a
Felipe <oliveira8.1felipe@gmail.com>
2026-06-04 16:10:06 -0300

    timeout sweetspot found and it's 30

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function desktopScrollEffect() {
     var scrollingTimeout = 0;
 
     if (navigator.userAgent.indexOf("Safari") != -1 && navigator.userAgent.indexOf("Chrome") == -1 && navigator.userAgent.indexOf("Chromium") == -1) {
-        scrollingTimeout = 25;
+        scrollingTimeout = 30;
     }
     if (navigator.userAgent.indexOf("Firefox") != -1 && navigator.userAgent.indexOf("Chrome") == -1 && navigator.userAgent.indexOf("Chromium") == -1) {
-        scrollingTimeout = 25;
+        scrollingTimeout = 30;
     }
 
     function syncScroll(event, source, targets, columns) {
